c727d2e404ca96e76a3fcc9ccb12f917aab56db9
Dhruvil <dhruvil.sudani10@gmail.com>
2022-03-22 12:01:37 +0530

    update form validation

--- add_user.php
+++ add_user.php
@@ -1,3 +1,6 @@
+<?php
+ require 'insert.php';
+?>
 <!doctype html>
 <html lang="en">
 
@@ -18,19 +21,28 @@
         width: 500px;
 
     }
+    .error{
+        color: red;
+    }
     </style>
 </head>
 
 <body>
     <div class="container">
-        <form action="/index.php" method="POST">
+        <form action="add_user.php" method="post" enctype="multipart/form-data">
             <div class="mb-3">
                 <label for="name" class="form-label">Name</label>
+                <span class="error">*
+                    <?php echo $nameErr;?>
+                </span>
                 <input type="char" class="form-control" id="username" name="username">
             </div>
 
             <div class="mb-3">
                 <label for="exampleInputEmail1" class="form-label">Email address</label>
+                <span class="error">*
+                    <?php echo $emailErr;?>
+                </span>
                 <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"
                     name="email">
                 <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
@@ -52,25 +64,21 @@
             </div>
 
             <div class="mb-3">
-
+                <br>
                 <input type="file" class="form-control" id="img" name="image">
-
+                
             </div>
             <button type="submit" class="btn btn-primary">Submit</button>
+            <button type="button" class="btn btn-secondary">Close</button>
         </form>
     </div>
-    <!-- Optional JavaScript; choose one of the two! -->
 
-    <!-- Option 1: Bootstrap Bundle with Popper -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous">
     </script>
 
-    <!-- Option 2: Separate Popper and Bootstrap JS -->
-    <!--
-    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js" integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js" integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" crossorigin="anonymous"></script>
-    -->
+
+
 </body>
 
 </html>
--- index.php
+++ index.php
@@ -1,4 +1,60 @@
-<?php
-echo 'hello world';
+<!doctype html>
+<html lang="en">
 
-?>
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+
+    <title>Hello, world!</title>
+    <style>
+
+    </style>
+</head>
+
+<body>
+
+
+    <div class="mb-3">
+        <a type="button" href="add_user.php" class="btn btn-success float-right">ADD</a>
+    </div>
+    <div class="container">
+        <table class="table table-bordered border-primary">
+            <tr>
+                <!-- <th>id</th> -->
+                <th>Name</th>
+                <th>Phone</th>
+                <th>Email</th>
+                <th>Gender</th>
+                <th>Image</th>
+                <th>Created date</th>
+            </tr>
+
+            <tr class="data">
+
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+
+            </tr>
+
+        </table>
+
+
+    </div>
+
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous">
+    </script>
+
+</body>
+
+</html>
